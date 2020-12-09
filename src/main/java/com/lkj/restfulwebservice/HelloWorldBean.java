482d0416125c107068abc99307321e60880efd93
이경진 <bramhome5848@gmail.com>
2020-12-09 16:59:17 +0900

    20201209 create project

---
+++ src/main/java/com/lkj/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.lkj.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+}
+
---
+++ src/main/java/com/lkj/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.lkj.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    /**
+     * Dispatcher Servlet
+     * client -> http request -> DispatchServlet -> Handler Mapping
+     * -> RestController(@Controller + @ResponseBody) -> http Response -> client
+     */
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");   //return Object -> return response body
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
