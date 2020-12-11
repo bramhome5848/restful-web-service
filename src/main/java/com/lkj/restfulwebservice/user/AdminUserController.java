a7d6c91e4ac00304a559986ca4bcb03ad5aaf791
이경진 <bramhome5848@gmail.com>
2020-12-11 12:16:36 +0900

    20201211 REST Api versioning

--- src/main/java/com/lkj/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/lkj/restfulwebservice/user/AdminUserController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -35,8 +36,16 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable Integer id) {
+    /**
+     * Uri, Request param versioning -> 일반 브라우저에서 실행 가능
+     * Media type, Headers versioning -> 일반 브라우저에서 실행 불가
+     */
+
+    //@GetMapping("/v1/users/{id}") //versioning
+    //@GetMapping(value = "/users/{id}", params = "version=1")    //request param versioning
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1") //header versioning
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json") //mime type versioning -> Accept
+    public MappingJacksonValue retrieveUserV1(@PathVariable Integer id) {
         User user = userService.findOne(id);
 
         if(user == null) {
@@ -50,6 +59,32 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    //@GetMapping("/v2/users/{id}") //versioning
+    //@GetMapping(value = "/users/{id}", params = "version=2")    //request param versioning
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json") //mime type versioning -> Accept
+    public MappingJacksonValue retrieveUserV2(@PathVariable Integer id) {
+        User user = userService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
--- src/main/java/com/lkj/restfulwebservice/user/User.java
+++ src/main/java/com/lkj/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -12,6 +13,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 //@JsonFilter -> Filter ID를 문자열로 지정, 해당 어노테이션을 사용하면 무조건 FilterProvider 와 해당 ID를 처리하는 필터를 제공해야 함
 @JsonFilter("UserInfo")
