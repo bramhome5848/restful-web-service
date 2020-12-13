1cc94aec14f4e735545dc612394c0d44d362fa4f
이경진 <bramhome5848@gmail.com>
2020-12-13 18:44:11 +0900

    20201213 Apply Swagger

---
+++ src/main/java/com/lkj/restfulwebservice/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.lkj.restfulwebservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * http://localhost:8088/v2/api-docs/
+ * http://localhost:8088/swagger-ui/
+ */
+@Configuration
+public class SwaggerConfig {
+
+    private static final Contact DEFAULT_CONTACT = new Contact("Kyungjin Lee",
+            " https://github.com/bramhome5848", "bramhome5848@gmail.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+            "My User management REST API service", "1.0", "urn:tos",
+            DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml"));
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+}
--- src/main/java/com/lkj/restfulwebservice/user/User.java
+++ src/main/java/com/lkj/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.lkj.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,17 +19,24 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 //@JsonFilter -> Filter ID를 문자열로 지정, 해당 어노테이션을 사용하면 무조건 FilterProvider 와 해당 ID를 처리하는 필터를 제공해야 함
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 객체") //swagger
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name 은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
+
     @Past   //미래 데이터를 쓸 수 없고 과거 데이터만
+    @ApiModelProperty(notes = "사용자 등록일을 입력해주세요.")
     private Date joinDate;
 
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 패스워드 입력해주세요.")
     private String password;
+
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
 }
